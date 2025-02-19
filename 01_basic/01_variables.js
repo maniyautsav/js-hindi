c3ad8730896206623a3fbbe083fd8fc0f61a45ff
utsavmaniya <maniyautsav99@gmail.com>
2025-02-19 11:02:34 +0000

    variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 124455
+let accountEmail = "utsavmaniya77@gmail.com"
+// var accountPassword = "9825"
+accountCity = "surat"
+let accountState = "Gujrat" 
+
+// accountId = 9888665
+
+accountEmail= "swati32@gmail.com"
+accountPassword= "9988"
+accountCity = "bhavnager"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
